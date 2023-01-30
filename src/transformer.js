8401b96ebd6c19dec22563a7cc64e081051fd4db
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2023-01-30 09:50:24 +0800

    feat: responsive slot and improvements (#6)
    * ci: pnpm typo
    
    * feat: responsive slot and improvements

--- src/transformer.js
+++ src/transformer.js
@@ -1,19 +1,21 @@
 // Ignore the `initial`
-// TODO: should respect tailwind config;
+// TODO: respect tailwind config
 const screens = ["xs", "sm", "md", "lg", "xl", "2xl"];
 
 const regExp = {
-  tv: new RegExp(/tv\({[\s\S]*?}\)/g),
-  tvContent: new RegExp(/\({[\s\S]*?}\)/g),
-  comment: new RegExp(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm),
-  blankLine: new RegExp(/^\s*$(?:\r\n?|\n)/gm),
+  tv: /tv\({[\s\S]*?}\)/g,
+  tvContent: /\({[\s\S]*?}\)/g,
+  comment: /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm,
+  blankLine: /^\s*$(?:\r\n?|\n)/gm,
 };
 
-const isEmpty = (content) => {
-  if (!content) return true;
-  if (Array.isArray(content) && content.length === 0) return true;
-  if (typeof content === "string" && content.length === 0) return true;
-  if (typeof content === "object" && Object.keys(content).length === 0) return true;
+const isArray = (param) => Array.isArray(param);
+
+const isEmpty = (param) => {
+  if (!param) return true;
+  if (isArray(param) && param.length === 0) return true;
+  if (typeof param === "string" && param.length === 0) return true;
+  if (typeof param === "object" && Object.keys(param).length === 0) return true;
 
   return false;
 };
@@ -26,24 +28,58 @@ const getCleanContent = (content) => {
 };
 
 const getTVObjects = (content) => {
-  const tvStrArray = getCleanContent(content);
+  const tvs = getCleanContent(content);
 
-  if (isEmpty(tvStrArray)) return;
+  if (isEmpty(tvs)) return;
 
-  return tvStrArray.map((item) => {
-    const str = item.toString();
-    const tv = str.match(regExp.tvContent).toString();
-
-    // TODO: `eval` should be avoided
-    return eval(
-      tv
-        .replaceAll("`", '"')
-        .replaceAll("'", '"')
-        .replaceAll(/\s{2,}/g, " ")
-        .replaceAll(/(\r\n|\n|\r)/gm, "")
-        .trim(),
-    );
+  return tvs.map((tv) => {
+    // TODO: should be improved to avoid potential security issues
+    return new Function(`return ${tv.match(regExp.tvContent).toString()}`)();
   });
+};
+
+const flatClassNames = (classNames) => {
+  return classNames
+    .flatMap((classNameSet) => classNameSet)
+    .toString()
+    .replaceAll(",", " ")
+    .split(" ");
+};
+
+const getSlots = (classNames) => {
+  // responsive slot
+  let rs = {};
+
+  for (const [slotName, slotClassNames] of Object.entries(classNames)) {
+    rs[slotName] = {};
+    rs[slotName].original = slotClassNames;
+
+    if (isEmpty(slotClassNames)) continue;
+
+    rs.temp = isArray(slotClassNames) ? flatClassNames(slotClassNames) : slotClassNames.split(" ");
+
+    screens.forEach((screen) => {
+      let tempClassNames = "";
+
+      rs.temp.forEach((className) => {
+        tempClassNames += `${screen}:${className} `;
+      });
+
+      rs[slotName][screen] = tempClassNames.trimEnd();
+    });
+
+    delete rs.temp;
+  }
+
+  return rs;
+};
+
+const getVariants = (classNames) => {
+  if (typeof classNames === "string") return classNames.split(" ");
+  if (isArray(classNames)) return flatClassNames(classNames);
+  if (classNames instanceof Object) return getSlots(classNames);
+
+  return classNames;
 };
 
 const transformContent = (tv) => {
@@ -53,30 +89,36 @@ const transformContent = (tv) => {
 
   let responsive = {};
 
-  for (const [variantKey, variant] of Object.entries(variants)) {
+  for (const [variantName, variant] of Object.entries(variants)) {
+    responsive[variantName] = {};
     if (isEmpty(variant)) continue;
 
-    responsive[variantKey] = {};
+    for (const [variantType, variantClassNames] of Object.entries(variant)) {
+      responsive[variantName][variantType] = {};
+      responsive[variantName][variantType].original = variantClassNames;
 
-    for (const [variantType, original] of Object.entries(variant)) {
-      if (isEmpty(original)) continue;
+      if (isEmpty(variantClassNames)) continue;
 
-      const originalClasses = original.split(" ");
+      const formattedClassNames = getVariants(variantClassNames);
 
-      if (isEmpty(originalClasses)) continue;
+      if (isEmpty(formattedClassNames)) continue;
 
-      responsive[variantKey][variantType] = {};
+      // slots
+      if (!isArray(formattedClassNames)) {
+        responsive[variantName][variantType] = formattedClassNames;
+        continue;
+      }
 
-      for (const screen of screens.values()) {
-        let responsiveClasses = ``;
+      // variants
+      screens.forEach((screen) => {
+        let tempClassNames = "";
 
-        originalClasses.forEach((item) => {
-          responsiveClasses += `${screen}:${item} `;
+        formattedClassNames.forEach((className) => {
+          tempClassNames += `${screen}:${className} `;
         });
 
-        responsive[variantKey][variantType].original = original;
-        responsive[variantKey][variantType][screen] = responsiveClasses.trimEnd();
-      }
+        responsive[variantName][variantType][screen] = tempClassNames.trimEnd();
+      });
     }
   }
 
@@ -86,23 +128,19 @@ const transformContent = (tv) => {
 const tvTransformer = (content) => {
   try {
     // TODO: support package alias
-    const isIncluded = content.includes("tailwind-variants");
-
-    if (!isIncluded) return content;
+    if (!content.includes("tailwind-variants")) return content;
 
     const tvs = getTVObjects(content);
 
     if (isEmpty(tvs)) return content;
 
-    const transformedContent = tvs.map((tv) => {
-      return transformContent(tv);
-    });
-
-    const concatedContent = content.concat(
-      `\n/*\n\n${JSON.stringify(transformedContent, undefined, 2)}\n\n*/\n`,
+    const transformed = JSON.stringify(
+      tvs.map((tv) => transformContent(tv)),
+      undefined,
+      2,
     );
 
-    return concatedContent;
+    return content.concat(`\n/*\n\n${transformed}\n\n*/\n`);
   } catch (error) {
     return content;
   }
