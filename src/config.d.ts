77989e3ae151935db8dac88090dae22b227488e9
Nick Sergeant <nick@nicksergeant.com>
2023-11-28 21:58:14 -0500

    Actually use strings instead.

--- src/config.d.ts
+++ src/config.d.ts
@@ -14,7 +14,7 @@ export type TWMConfig = {
    * The config object for `tailwind-merge` library.
    * @see https://github.com/dcastil/tailwind-merge/blob/v1.8.1/docs/configuration.md
    */
-  twMergeConfig?: Partial<TwMergeConfig<never, never>>;
+  twMergeConfig?: Partial<TwMergeConfig<string, string>>;
 };
 
 export type TVConfig<
